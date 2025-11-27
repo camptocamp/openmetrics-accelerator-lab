052b02d8180572e372012b513e14c9f68c68879b
Julien Acroute <julien.acroute@camptocamp.com>
2025-11-27 11:30:34 +0100

    feat: add beam energy

--- src/app.py
+++ src/app.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 # --- Prometheus metrics ---
 kick_power = Gauge("kick_power", "Kick power")
 beam_speed = Gauge("beam_speed_km_s", "Current beam speed in km/s")
+beam_energy = Gauge("beam_energy_mev", "Current beam energy in MeV")
 accelerator_state = Gauge("accelerator_state", "Accelerator state: 0=stopped, 1=running, 2=success, 3=overloaded, 4=timeout")
 
 # --- Internal state ---
 state = 0
 KICK_POWER = 27 # arbitrary unit, should be stay in [0;100]
+c = 299_792_458  # speed of light
+m0 = 0.000938  # proton energy in TeV
 
 @app.route("/")
 def index():
@@ -30,6 +33,7 @@ def update_state():
 
     # Update metrics values    
     beam_speed.set(speed)
+    beam_energy.set(m0*math.sqrt((c/2*abs(c - speed*1000))))
     accelerator_state.set(status)
     if(status == 1): # status=running
         kick_power.set(KICK_POWER)
